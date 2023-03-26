b6103defdd989488201d16767c7cce211fe56eda
Aish1214 <funaguskaraishwarya@gmail.com>
2023-03-27 01:05:55 +0600

    Add files via upload

---
+++ natural_number_or_not.c
@@ -0,0 +1,16 @@
+// wap to check whether the number is natural or not
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("Enter the number: ");
+ scanf("%d", &a);
+ if(a >= 1)
+{
+    printf("It is natural number");
+
+}else{
+    printf("Wrong Input");
+}
+return 0;
+}
