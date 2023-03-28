698f93d21dee917f3c9f451225c7fb85a0d1d8a2
Aish1214 <funaguskaraishwarya@gmail.com>
2023-03-29 01:20:57 +0600

    Add files via upload

---
+++ continue_practice_problem.c
@@ -0,0 +1,12 @@
+//print all numbers from 1 to 10 expect 6
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=10;i++){
+        if(i == 6){
+            continue;
+        }
+        printf("%d \n", i);
+    }
+    return 0;
+}
